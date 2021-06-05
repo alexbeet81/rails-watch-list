d1d0257836297f06e421aeca2a7ca8dee47846ba
Alex Pritchard <alexbeet1234@gmail.com>
2021-06-05 15:14:30 +0800

    removed js and added new images

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["find your favourite movies", "add comments",
-              "share them with your friends!"],
+              "share them with your friends", "grab some popcorn", "sit down and watch!"],
     typeSpeed: 50,
     loop: true
   });
